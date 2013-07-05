b9d21e029d64b4a60d1a976934f9de09a0546f20
serbanu <serban.stefanita@gmail.com>
2013-07-06 02:50:16 +0300

    Class who defines a person

---
+++ Persoana.java
@@ -0,0 +1,21 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Vladx
+ * Date: 7/4/13
+ * Time: 10:05 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Persoana {
+
+    String nume;
+    String prenume;
+    float CNP;
+    int varsta;
+
+    public Persoana(String nume, String prenume, float CNP, int varsta) {
+    	this.nume = nume;
+    	this.prenume = prenume;
+    	this.CNP = CNP;
+    	this.varsta = varsta;
+    }
+}
